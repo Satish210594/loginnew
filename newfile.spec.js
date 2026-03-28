ed6d0ca56486e3d0f0ee9ab68233a83910974826
Satish210594 <satishsathya94@gmail.com>
2026-03-28 12:33:59 +0530

    login

---
+++ newfile.spec.js
@@ -0,0 +1,8 @@
+import {test} from '@playwright/test';
+
+test('login',async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/");
+    await page.locator("#username").fill("satish");
+    await page.locator
+
+})
